9095742a198f79f6fe680d6b549c002030205442
Mikulew <mikulew@gmail.com>
2017-03-14 16:45:51 +0100

    first commit

---
+++ app/scripts/main.js
@@ -0,0 +1,30 @@
+
+
+$(document).ready(function() {
+  var $menu = $(".hamburger");
+  var $navbar = $('.navbar');
+
+  $menu.click('click', toggleMenu);
+
+  function toggleMenu() {
+    $menu.toggleClass('open');
+    $navbar.toggleClass('open');
+  }
+
+  $('.navlabel').on('click', scrollTo);
+
+  function scrollTo(e) {
+    e.preventDefault();
+
+    var target = this.hash;
+    var $target = $(target);
+
+    $('html, body').animate({
+      'scrollTop': $target.offset().top
+    }, 900, 'swing');
+    if($menu.hasClass('open') && $navbar.hasClass('open')) {
+      $menu.removeClass('open');
+      $navbar.removeClass('open');
+    }
+  }
+});
